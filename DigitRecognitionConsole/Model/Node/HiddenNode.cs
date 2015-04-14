926199841689809fee7d962523c586f1e128d8ea
Nayshlok <nineborland@gmail.com>
2015-04-14 12:12:07 -0600

    missing hidden node

---
+++ DigitRecognitionConsole/Model/Node/HiddenNode.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitRecognitionConsole.Model
+{
+    public class HiddenNode : ActivatingNode
+    {
+        public double AdjustWeights()
+        {
+            double WeightedError = 0;
+            foreach (NetConnection nc in Outputs)
+            {
+                WeightedError += nc.Receiver.Error * nc.Weight;
+            }
+            double activation = activationFunction();
+            double error = activation * (1 - activation) * WeightedError;
+
+            foreach (NetConnection nc in Inputs)
+            {
+                nc.Weight = nc.Weight + (LEARNING_RATE * error * nc.Sender.Activation);
+            }
+
+            return error;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", A: " + Activation + ", E: " + Error;
+        }
+
+    }
+}
